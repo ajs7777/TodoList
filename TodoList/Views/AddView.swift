5f7e0249e541a943662238b96b980ea781381bdc
ajs7777 <abhisaha955@gmail.com>
2024-12-13 14:14:26 +0530

    Add an alert,  whaen an user input less than 3 characters it shows an alert

--- TodoList/Views/AddView.swift
+++ TodoList/Views/AddView.swift
@@ -14,13 +14,15 @@ struct AddView: View {
     @EnvironmentObject var listviewModel: ListViewModel
     
     @State var textFieldText: String = ""
+    @State var showAlert: Bool = false
+    @State var alertTitle: String = ""
     
     var body: some View {
         ScrollView {
             VStack{
                 TextField(text: $textFieldText) {
                     Text("Type Something...")
-                      
+                    
                 }
                 .padding()
                 .frame(height: 60)
@@ -37,18 +39,36 @@ struct AddView: View {
                         .background(Color.blue)
                         .cornerRadius(10)
                 }
-
+                
                 
             }
             .padding()
         }
         .navigationTitle(Text("Add an Item"))
-               
+        .alert(isPresented: $showAlert) {
+            getAlert()
+        }
+        
     }
     
     func saveButtonPressed() {
-        listviewModel.addItem(title: textFieldText)
-        dismiss()
+        if textIsAppropiate() {
+            listviewModel.addItem(title: textFieldText)
+            dismiss()
+        }
+        
+    }
+    
+    func textIsAppropiate() -> Bool {
+        if textFieldText.count < 3 {
+            alertTitle = "Title must be at least 3 characters long"
+            showAlert.toggle()
+            return false
+        }
+        return true
+    }
+    func getAlert() -> Alert {
+        Alert(title: Text(alertTitle), message: Text("enter 6 characers"), dismissButton: .default(Text("OK")))
     }
 }
 
