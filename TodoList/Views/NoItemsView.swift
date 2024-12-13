d6adf15e0df5df4a88d3f2dc7508905ae82626cb
ajs7777 <abhisaha955@gmail.com>
2024-12-13 17:52:11 +0530

    added user defaults for saving data and added a no item view, when the items list is empty then show this view

---
+++ TodoList/Views/NoItemsView.swift
@@ -0,0 +1,18 @@
+//
+//  NoItemsView.swift
+//  TodoList
+//
+//  Created by Abhijit Saha on 13/12/24.
+//
+
+import SwiftUI
+
+struct NoItemsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    NoItemsView()
+}
